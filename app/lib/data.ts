95eb03ced57bca148be17697d93a9dd5166ebaef
Nitiraj Rathore <nitiraj.rathore@gmail.com>
2024-12-11 13:56:31 +0530

    fixed the debbuging not working issue. And also fixed the database not getting connected issue.

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,4 +1,6 @@
 import { sql } from '@vercel/postgres';
+import { db } from "@vercel/postgres";
+
 import {
   CustomerField,
   CustomersTableType,
@@ -9,6 +11,8 @@ import {
 } from './definitions';
 import { formatCurrency } from './utils';
 
+const client = await db.connect();
+
 export async function fetchRevenue() {
   try {
     // Artificially delay a response for demo purposes.
@@ -17,7 +21,7 @@ export async function fetchRevenue() {
     // console.log('Fetching revenue data...');
     // await new Promise((resolve) => setTimeout(resolve, 3000));
 
-    const data = await sql<Revenue>`SELECT * FROM revenue`;
+    const data = await client.sql<Revenue>`SELECT * FROM revenue`;
 
     // console.log('Data fetch completed after 3 seconds.');
 
