4e22ac7166e530d277f58cc55c39f6c7495fc960
Nitiraj Rathore <nitiraj.rathore@gmail.com>
2024-12-15 19:27:47 +0530

    completing login logout and adding SEO tags and metadata

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -13,6 +13,16 @@ import { formatCurrency } from './utils';
 
 const client = await db.connect();
 
+export async function getUser(email: string): Promise<User | undefined> {
+  try{
+    const user = await client.sql<User>`SELECT * FROM users WHERE email=${email}`;
+    return user.rows[0];
+  } catch (error) {
+    console.error('Failed to fetch user:', error);
+    throw new Error('Failed to fetch user.');
+  }
+}
+
 export async function insertInvoiceInDB(customerId: string, amountInCents: number, status: string, date: string) {
   try {
     await client.sql`
---
+++ auth.ts
@@ -0,0 +1,34 @@
+import NextAuth from "next-auth";
+import { authConfig } from "./auth.config";
+import {z} from 'zod';
+import Credentials from 'next-auth/providers/credentials';
+import {getUser} from './app/lib/data';
+import bcrypt from 'bcrypt';
+
+
+export const {auth, signIn, signOut} = NextAuth({
+    ...authConfig,
+    providers:[Credentials({
+        async authorize(credentials) {
+            const parsedCredentials = z.object({
+                email: z.string().email(),
+                password: z.string().min(6)
+            }).safeParse(credentials);
+
+            if (parsedCredentials.success) {
+                const {email, password} = parsedCredentials.data;
+                const user = await getUser(email);
+                if (!user) return null;
+
+                const passwordMatch = await bcrypt.compare(password, user.password);
+
+                if (passwordMatch) return user;
+            }
+
+            console.log('Invalid credentials');
+            return null;
+        },
+    }),
+   ],
+});
+
